aafcc8a004f0bf728818a56a35e04c7b7358cd7d
ConstantjuniorAmos <djimadoumadji-constant-junior.amos@laplateforme.io>
2025-07-06 22:17:32 +0200

    first commit 2

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ firebase_admin.initialize_app(FIREBASE_CREDENTIAL)
 app = Flask(__name__)
 app.register_blueprint(chatbot_bp, url_prefix="/api")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
